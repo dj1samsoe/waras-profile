c2db616bc43eb66028e2c2c22bc1289509f6b92c
dj1samsoe <dhanyh86@gmail.com>
2024-01-25 19:44:33 +0700

    refactor: add metadata keywords

--- common/constant/metadata.ts
+++ common/constant/metadata.ts
@@ -1,9 +1,9 @@
 export const METADATA = {
   creator: "Kursus Mengemudi Waras",
   description:
-    "Kursus mengemudi Waras mengajarkan teori dan praktik mengemudi untuk pemula sampai mahir.",
+    "Kursus mengemudi Waras adalah kursus mengemudi terbaik di Krian, Sidoarjo. Kami mengajarkan teori dan praktik mengemudi untuk pemula sampai mahir.",
   keyword:
-    "Kursus Mengemudi, Kursus Mengemudi Waras, Kursus Mengemudi Mobil, Kursus Mengemudi Mobil Sidoarjo, Kursus Mengemudi Mobil Krian, Kursus Mengemudi Mobil Waras, Kursus Mengemudi Mobil Waras Sidoarjo, Kursus Mengemudi Mobil Waras Krian, Kursus Mengemudi Mobil Waras, Kursus Mengemudi Waras",
+    "Kursus Mengemudi, Kursus Mengemudi Waras, Kursus Mengemudi Mobil, Kursus Mengemudi Mobil Sidoarjo, Kursus Mengemudi Mobil Krian, Kursus Mengemudi Mobil Waras, Kursus Mengemudi Mobil Waras Sidoarjo, Kursus Mengemudi Mobil Waras Krian, Kursus Mengemudi, Waras, Kursus, Mengemudi, Sidoarjo, Krian,",
   authors: {
     name: "Kursus Mengemudi Waras",
     url: process.env.DOMAIN,
